a3a5d56c5779ed6b3bd74e1ddba3604af7d37527
scollo26 <giovanniscollo26@gmail.com>
2022-02-16 16:27:13 +0100

    fascia di prezzo

--- classi/FasciaUtente.php
+++ classi/FasciaUtente.php
@@ -1,6 +1,73 @@
 <?php 
 
+    class FasciaUtente extends Utente {
 
+        public $sconto;
+        public $Type;
+
+
+        public function __construct($Type)
+        {
+            $this->setType($Type);
+            // $this->sconto ="non hai sconti" . $this->sconto;
+        }
+
+        public function addtype($Type){
+            if($Type == "bronzo"){
+
+                $this->sconto = 40;
+
+            }elseif($Type == "argento"){
+
+                $this-> sconto = 50;
+
+            }elseif($Type == "oro"){
+
+                $this->sconto = 80;
+            }
+            return $this;
+        }
+
+        /**
+         * Get the value of sconto
+         */ 
+        public function getSconto()
+        {
+                return $this->sconto;
+        }
+
+        /**
+         * Set the value of sconto
+         *
+         * @return  self
+         */ 
+        public function setSconto($sconto)
+        {
+                $this->sconto = $sconto;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of Type
+         */ 
+        public function getType()
+        {
+                return $this->Type;
+        }
+
+        /**
+         * Set the value of Type
+         *
+         * @return  self
+         */ 
+        public function setType($Type)
+        {
+                $this->Type = $Type;
+
+                return $this;
+        }
+    }
 
 
 
--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php 
 
-require_once 'classi/Utente.php';
-require_once 'classi/Prodotti.php';
+require_once __DIR__ . '/classi/Utente.php';
+require_once __DIR__ . '/classi/Prodotti.php';
+require_once __DIR__ . '/classi/FasciaUtente.php';
 
-// require_once 'classi/fasciaUtente.php';
-// require_once 'classi/card.php';
 
 $utente = new Utente('Oriana', 'Avitabile', 'oriana@mail.it', 'Via Etnea', 'Milano', new Card('16/02/2022',2627637,323)); 
-var_dump($utente);
+
+$product = new Prodotti ("gel","per capelli","nivea", 20);
+// var_dump($utente,$product);
+
+$sconto = new FasciaUtente("bronzo");
+var_dump($sconto->getSconto());
 
 
 $utenteBronze = new Utente('Patrizia', 'Masci', 'patrizia@mail.com', 'Via crisafulli', 'Roma', new Card('16/02/2022',54567637,723)); 
     
-var_dump($utenteBronze);
+
 
 
 $utenteSilver = new Utente('Davide', 'Di Mauro', 'davide@mail.it', 'Via Marco ', 'Milano', new Card('16/02/2022',262532737,923)); 
-var_dump($utenteSilver);
+
     
 
 $utentePremium = new Utente('Luca', 'Guarnera', 'luca@mail.com', 'Via Galermo', 'Catania', new Card('16/02/2022',5347657,423)); 
-var_dump($utentePremium);
+
 
 
 
